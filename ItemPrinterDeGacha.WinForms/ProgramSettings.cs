ee5e8fe3ab1017e5df2977707ad23790fe4db22c
间辞 <123729062+jcx521lj1315@users.noreply.github.com>
2024-04-16 21:36:19 +0800

    Update ProgramSettings.cs

--- ItemPrinterDeGacha.WinForms/ProgramSettings.cs
+++ ItemPrinterDeGacha.WinForms/ProgramSettings.cs
@@ -2,7 +2,7 @@ namespace ItemPrinterDeGacha.WinForms;
 
 public sealed class ProgramSettings
 {
-    public string Language { get; init; } = "en";
+    public string Language { get; init; } = "zh";
     public string TimeFormat { get; init; } = "yyyy-MM-dd HH:mm:ss";
     public bool DisableDpiScaling { get; init; } // Only set to true if you want to use Font scaling
     public int CurrentTab { get; set; }
